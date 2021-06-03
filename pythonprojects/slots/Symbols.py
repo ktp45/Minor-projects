20cf88b09f7d28f04752070f432ec1cc257a13ab
ktp45 <61913665+ktp45@users.noreply.github.com>
2021-06-03 17:51:20 +0300

    Add files via upload

---
+++ pythonprojects/slots/Symbols.py
@@ -0,0 +1,8 @@
+class Symbol:  # each symbol has name, in-game multiplier and can be the universal symbol (wild)
+    def __init__(self, name=" ", multiplier=0., isWild=False):
+        self.name = name
+        self.multiplier = multiplier
+        self.isWild = isWild
+
+    def __str__(self):  # we want to print just the symbols name
+        return self.name
